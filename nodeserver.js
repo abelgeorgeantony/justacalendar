edc0c68d2d729ccf25beb3f8d7b0fef72a1c0ab0
abelgeorgeantony <abelgeorgeantony@gmail.com>
2024-06-22 23:12:28 +0530

    First contact with gemini api. Has hidden API keys using .env file which is not tracked so added a .gitignore file for that.

--- nodeserver.js
+++ nodeserver.js
@@ -1,11 +1,17 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
 const url = require("url");
 const path = require("path");
 const express = require("express");
 const app = express();
 
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "calendar.html"));
+	run();
 });
 app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, "calendar.html"));
@@ -37,3 +43,15 @@ app.listen(portnumber, address, () => {
     console.log("Server running in address: "+address);
     console.log("Port: "+portnumber);
 });
+
+
+
+async function run() {
+  const prompt = "Pick a date in history VERY RANDOMLY and a fun or factual info related to it. ASSERTING THAT THE INFORMATION SHOULD BE ACCURATELY RELATED WITH THE RANDOMLY CHOSEN DATE. Format for output:<yyyy/mm/dd>;<Information relating the date>";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
